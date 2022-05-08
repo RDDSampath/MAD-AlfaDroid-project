d6a2fba05f4b67b37c5274e963c72190a62fe944
Dhanushka <it20646592@my.sliit.lk>
2022-05-08 09:30:35 +0530

    3rd commit

---
+++ app/src/main/java/com/dhanushka/timetable/fragments/SundayFragment.java
@@ -0,0 +1,44 @@
+package com.dhanushka.timetable.fragments;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.dhanushka.timetable.adapters.WeekAdapter;
+import com.dhanushka.timetable.utils.DbHelper;
+import com.dhanushka.timetable.R;
+import com.dhanushka.timetable.utils.FragmentHelper;
+
+
+
+public class SundayFragment extends Fragment {
+
+    public static final String KEY_SUNDAY_FRAGMENT = "Sunday";
+    private DbHelper db;
+    private ListView listView;
+    private WeekAdapter adapter;
+    private int listposition = 0;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sunday, container, false);
+        setupAdapter(view);
+        setupListViewMultiSelect();
+        return view;
+    }
+
+    private void setupAdapter(View view) {
+        db = new DbHelper(getActivity());
+        listView = view.findViewById(R.id.sundaylist);
+        adapter = new WeekAdapter(getActivity(), listView, R.layout.listview_week_adapter, db.getWeek(KEY_SUNDAY_FRAGMENT));
+        listView.setAdapter(adapter);
+    }
+
+    private void setupListViewMultiSelect() {
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        listView.setMultiChoiceModeListener(FragmentHelper.setupListViewMultiSelect(getActivity(), listView, adapter, db));
+    }
+}
